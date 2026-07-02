52cda8bbc73619116f5d8a723f3f92e4d2def367
Lupy Ariesta <lupyariestaa@gmail.com>
2026-07-02 16:32:32 +0700

    Update next.config.mjs with typescript and eslint options
    Added typescript and eslint configuration options to ignore build errors and linting during builds.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    remotePatterns: [],
-  },
+  images: { remotePatterns: [] },
   experimental: {
     optimizePackageImports: ["lucide-react", "recharts"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
